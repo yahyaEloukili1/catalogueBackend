cbe906f09eed08d7b78e30af8a8b54950602be13
yahya <yahya.ensas@hotmail.com>
2021-04-20 09:00:10 +0000

    cross origin

--- src/main/java/org/ensas/catalogue/dao/ProduitRepository.java
+++ src/main/java/org/ensas/catalogue/dao/ProduitRepository.java
@@ -8,8 +8,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestResource
+@CrossOrigin("*")
 public interface ProduitRepository extends JpaRepository<Produit, Long> {
 	@RestResource(path = "/byDesignation")
 	public List<Produit> findByDesignationContains(@Param("mc") String des);
